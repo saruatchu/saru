f7bc8eb587208a2cdf061c5b0d3044edccb53955
saruatchu <sarumathiayyappan@gmail.com>
2017-06-28 14:40:40 +0530

    Update saru.c

--- saru.c
+++ saru.c
@@ -1,1 +1,15 @@
-sdtyr
+#include<stdio.h>
+#include<conio.h>
+voidmain()
+{
+  int a;
+  clrscr();
+  printf("enter a number");
+  scanf("%d",&a);
+  if(a>0)
+    printf("the number is postive");
+  else
+    printf("the number is negative");
+  getch();
+}
+        
