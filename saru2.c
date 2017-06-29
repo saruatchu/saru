7ecd2e6dec29e40f91c63a6ec23b52e69b3c2cee
saruatchu <sarumathiayyappan@gmail.com>
2017-06-29 13:57:20 +0530

    Rename saru1.c to saru2.c

---
+++ saru2.c
@@ -0,0 +1,14 @@
+#include<conio.h>
+#include<stdio.h>
+void main()
+{
+int a;
+clrscr();
+printf("enter the number");
+scanf("%d",&a);
+if(a%2==0)
+printf("the number is even");
+else
+printf("the number is odd");
+getch();
+}
